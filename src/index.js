691bd99a88a561b25d0dec7d98a419792f106b55
Ivan Trofimov <ivan@trofimov.link>
2018-09-27 11:29:16 +0500

    feat(middlewares/error): added error middleware

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import Koa from 'koa';
 import compose from 'koa-compose';
 
 import { routes, allowedMethods } from './middlewares/router';
+import errors from './middlewares/error';
 
 const app = new Koa();
 
 app.use(compose([
+  errors,
   routes,
   allowedMethods
 ]));
