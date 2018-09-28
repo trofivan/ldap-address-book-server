5cb48b26b14c58fb112f8237fe1af0f33644330b
Ivan Trofimov <ivan@trofimov.link>
2018-09-29 01:48:29 +0500

    refactoring(router.js): removed comments

--- src/middlewares/router.js
+++ src/middlewares/router.js
@@ -6,15 +6,12 @@ import {getUsers, getUser} from '../controllers/users';
 const router = new Router();
 
 router
-  // Companies
   .get('/companies', getCompanies)
   .get('/companies/:id', getCompany)
 
-  // Users
   .get('/users', getUsers)
   .get('/users/:id', getUser)
 
-  // 404
   .get('*', (ctx) => ctx.throw(404, 'Resource not found'));
 
 export const routes = router.routes();
